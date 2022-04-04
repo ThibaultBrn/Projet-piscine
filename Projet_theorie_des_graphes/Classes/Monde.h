4177e5bad78ed9ea3c9370318fd6e29a9d6fcecb
pironx <timothee.piron@gmail.com>
2022-04-04 16:56:28 +0200

    classe monde creer

---
+++ Projet_theorie_des_graphes/Classes/Monde.h
@@ -0,0 +1,14 @@
+#ifndef MONDE_H_INCLUDED
+#define MONDE_H_INCLUDED
+
+class Monde
+{
+    private:
+        std::vector<Aeroport*>m_aeroport;
+        std::vector<Avion*>m_avion;
+    public:
+
+
+};
+
+#endif // MONDE_H_INCLUDED
